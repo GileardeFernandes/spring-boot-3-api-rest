5d93ff9ecf0a39f1600fb74491db52666ffbad13
MicaelFernandes <gileardefernandes@gmail.com>
2023-01-05 00:00:17 -0300

    padronizando  tratamento de erros

---
+++ src/main/java/med/voll/api/infra/TraradorDeErros.java
@@ -0,0 +1,29 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TraradorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosFieldErro::new));
+    }
+
+    private record  DadosFieldErro(String compo,String mensagem ){
+        public  DadosFieldErro(FieldError fieldError){
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
